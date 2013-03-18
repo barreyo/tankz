db1c899d7e17cac219f92b3cc61b686b210b17b3
DANIEL BACKSTROM <danbacks@student.chalmers.se>
2013-03-18 14:34:41 +0100

    Added som basic empty classes

---
+++ src/TanksController/TanksMainController.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TanksController;
+
+/**
+ *
+ * @author Daniel
+ */
+public class TanksMainController {
+    
+}
