ab3c87d4c893b62783c671496b59bd00fda29bfd
DANIEL BACKSTROM <danbacks@student.chalmers.se>
2013-04-03 14:51:51 +0200

    Performance improved, effect spatials and projectiles are now removed properly with timers

--- src/GameLogicLayer/controls/TankProjectileControl.java
+++ src/GameLogicLayer/controls/TankProjectileControl.java
@@ -15,10 +15,11 @@ import com.jme3.effect.shapes.EmitterSphereShape;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
+import com.jme3.scene.Node;
 import com.jme3.scene.Spatial;
 
 /**
- * WIll BE EDITED
+ * 
  *
  * @author Daniel
  */
@@ -26,14 +27,20 @@ public class TankProjectileControl extends BaseControl implements PhysicsCollisi
     private PhysicsSpace physicsSpace;
     private MissileProjectile projectile;
     private RigidBodyControl physicsControl;
+    private ParticleEmitter effect;
     
- 
+    private boolean effectPlaying;
+    private static final float EFFECT_END_TIME = 0.5f;
+    private static final float MAX_LIFE_TIME = 4f;
     
+    private float effectTimer;
+    private float projectileLifeTimer;
     
     public TankProjectileControl() {
         TanksGame app = TanksGame.getApp();
         physicsSpace = app.getBulletAppState().getPhysicsSpace();
         physicsSpace.addCollisionListener(this);
+        effect = EEffects.EXPLOSION.getEmitter();
     }
     
      /**
@@ -55,28 +62,45 @@ public class TankProjectileControl extends BaseControl implements PhysicsCollisi
      */
     @Override
     public synchronized void collision(PhysicsCollisionEvent event) {
-        if (physicsSpace == null || spatial == null) {
+        if (spatial == null || projectile == null) {
             return;
         }
         physicsControl = projectile.getPhysicsControl();
         if (event.getObjectA() == physicsControl || event.getObjectB() == physicsControl) {
-            ParticleEmitter effect = EEffects.EXPLOSION.getEmitter();
             if (effect != null && spatial.getParent() != null) {
+                effectPlaying = true;
                 effect.setLocalTranslation(spatial.getLocalTranslation());
                 spatial.getParent().attachChild(effect);
-                effect.emitAllParticles();
+                effect.emitAllParticles();  
             }
             physicsSpace.remove(physicsControl);
             spatial.removeFromParent();
-            //physicsSpace.removeCollisionListener(this);
             projectile.cleanup();
+            effect.addControl(this);
         }
     }
 
     @Override
-    synchronized void controlUpdate(float tpf) {
-        Vector3f oldLocation = spatial.getWorldTranslation();
-        Vector3f newLocation = oldLocation.addLocal(projectile.getDirection());
-        spatial.setLocalTranslation(newLocation);
+    void controlUpdate(float tpf) {
+        if(enabled){
+            if (projectile != null && spatial != null) {
+                Vector3f oldLocation = spatial.getWorldTranslation();
+                Vector3f newLocation = oldLocation.addLocal(projectile.getDirection().normalizeLocal());
+                spatial.setLocalTranslation(newLocation);
+                projectileLifeTimer += tpf;
+                if (projectileLifeTimer > MAX_LIFE_TIME) {
+                    physicsSpace.remove(physicsControl);
+                    spatial.removeFromParent();
+                    projectile.cleanup();
+                }
+            }
+            if (effectPlaying) {
+                effectTimer += tpf;
+                if (effectTimer > EFFECT_END_TIME) {
+                    effect.removeFromParent();
+                    effect.removeControl(this);
+                }
+            }
+        }
     }
 }
