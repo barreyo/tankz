4f064651d0952464f26c963cc31b98db4e281607
PerThoresson <pthoresson@gmail.com>
2013-04-18 20:20:25 +0200

    Albin forces me to push his merge..........

--- src/GameView/effects/DestroyedEffect.java
+++ src/GameView/effects/DestroyedEffect.java
@@ -4,8 +4,12 @@
  */
 package GameView.effects;
 
+import App.TanksAppAdapter;
+import com.jme3.asset.AssetManager;
 import com.jme3.effect.ParticleEmitter;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  *
@@ -13,6 +17,12 @@ import java.util.Collection;
  */
 public class DestroyedEffect implements IEffect {
 
+    private List<ParticleEmitter> emitters = new ArrayList<ParticleEmitter>();
+    
+    public DestroyedEffect(){
+        AssetManager assetManager = TanksAppAdapter.INSTANCE.getAssetManager();
+    }
+    
     @Override
     public Collection<ParticleEmitter> getParticleEmitters() {
         throw new UnsupportedOperationException("Not supported yet.");
