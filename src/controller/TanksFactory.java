7088c82ec1f928cb92a0c5bc0365e619cc3109fa
Johan Backman <backman@hotmail.se>
2013-05-26 22:17:46 +0200

    Launch file in LAUNCHER folder.

--- src/controller/TanksFactory.java
+++ src/controller/TanksFactory.java
@@ -174,9 +174,7 @@ public final class TanksFactory {
             tmp.add(getNewPowerup(LandminePowerup.class));
             tmp.add(getNewBeerPowerup(players));
             tmp.add(getNewPowerup(HealthPowerup.class));
-            if (i % 2 == 0) {
-                tmp.add(getNewPowerup(AirCallPowerup.class));
-            }
+            tmp.add(getNewPowerup(AirCallPowerup.class));
         }
         return tmp;
     }
